a55448438485e7047e6b943a52ba44b4676259cc
Mynul Islam <mynul.sakil@gmail.com>
2024-06-29 20:03:31 +0600

    finished buy and sell stock

--- LeetCode/buyandsellstock2.js
+++ LeetCode/buyandsellstock2.js
@@ -1,5 +1,16 @@
-var maxProfit = function(prices) {
-    
+var maxProfit = function (prices) {
+  let maxProfit = 0;
+  let minPrice = 0;
+  const len = prices.length;
+
+  for (let i = 0; i < len - 1; i++) {
+    // If the current price is lower than the next price, buy the stock and sell it at the next price
+    if (prices[i] < prices[i + 1]) {
+      minPrice = prices[i];
+      maxProfit += prices[i + 1] - minPrice;
+    }
+  }
+  return maxProfit;
 };
-const result = maxProfit([7, 1, 5, 3, 6, 4]);
+const result = maxProfit([1, 2, 3, 4, 5]);
 console.log(result); // Expected output: 7
