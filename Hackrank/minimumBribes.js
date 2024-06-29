bd33f60f9982c15cac1360ef5214e4c81809f2ed
Mynul Islam <mynul.sakil@gmail.com>
2024-06-29 22:57:13 +0600

    it's work but time limit problem

--- Hackrank/minimumBribes.js
+++ Hackrank/minimumBribes.js
@@ -1,16 +1,27 @@
-
 function minimumBribes(q) {
-    // Write your code here
-    let bribes = 0;
-    for(let i=0;i<q.length-1;i++){
-    for(let j=i+1;j<q.length;j++){
-        if(q[i]>q[j]){
-            swap(q[i],q[j]);
-            bribes++;
-        }
+  // Write your code here
+  const bribesArray = new Array(q.length + 1).fill(0);
+  let bribes = 0;
+  for (let i = 0; i < q.length - 1; i++) {
+    for (let j = i + 1; j < q.length; j++) {
+      if (q[i] > q[j]) {
+        bribesArray[q[i]] = bribesArray[q[i]] + 1;
+        let temp = q[i];
+        q[i] = q[j];
+        q[j] = temp;
+        // console.log("bribesArray:", bribesArray, "q[i]:", q[i]);
+        bribes++;
+      }
     }
-    console.log(q);
-    return bribes;
-
+  }
+  for (let i = 0; i < bribesArray.length; i++) {
+    if (bribesArray[i] > 2) {
+      console.log("Too chaotic");
+      return;
+    }
+  }
+  console.log(bribes);
+  return;
 }
-const result=minimumBribes([1, 2, 5, 3 ,4 ,7, 8, 6]);
+const result = minimumBribes([2, 5, 1, 3, 4]);
+console.log(result); // Expected output: 4
