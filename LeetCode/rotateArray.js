74ac5c4f806553b5ec6eaa6ecb6cc72c06352634
Mynul Islam <mynul.sakil@gmail.com>
2024-06-29 06:54:25 +0600

    adding comment

--- LeetCode/rotateArray.js
+++ LeetCode/rotateArray.js
@@ -1,14 +1,15 @@
 var rotate = function (nums, k) {
-  // creating a new array
+  // creating a new array to store the rotated array
   const newArray = new Array(nums.length + k);
   const len = nums.length;
+  //   shifting the array to the right and storing it in the new array
   for (let i = 0; i < len; i++) {
     newArray[(i + k) % len] = nums[i];
   }
+  //   copying the new array to the original array
   for (let i = 0; i < len; i++) {
     nums[i] = newArray[i];
   }
-  console.log(nums)
   return;
 };
 const result = rotate([1, 2, 3, 4, 5, 6, 7], 3);
