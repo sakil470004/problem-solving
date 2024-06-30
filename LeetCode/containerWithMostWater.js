4a9dd28eeaca3948944364ad65f8f2400d38e9c1
Mynul Islam <mynul.sakil@gmail.com>
2024-06-30 20:16:07 +0600

    fixed issue with naming

--- LeetCode/containerWithMostWater.js
+++ LeetCode/containerWithMostWater.js
@@ -1,14 +1,14 @@
-var containerWithMostWater = function (heights) {
+var maxArea = function (heights) {
   const len = heights.length;
   let left = 0;
   let right = len - 1;
-  let maxArea = 0;
+  let maxAreaa = 0;
   while (left < right) {
     const width = right - left;
     const height = Math.min(heights[left], heights[right]);
     const area = width * height;
-    if (area > maxArea) {
-      maxArea = area;
+    if (area > maxAreaa) {
+      maxAreaa = area;
     }
     if (heights[left] < heights[right]) {
       left++;
@@ -16,8 +16,8 @@ var containerWithMostWater = function (heights) {
       right--;
     }
   }
-  return maxArea;
+  return maxAreaa;
 };
 
-const result = containerWithMostWater([1,14,6,2,5,4,14,3,7]);
+const result = maxArea([1,14,6,2,5,4,14,3,7]);
 console.log(result); // Expected output: 49
