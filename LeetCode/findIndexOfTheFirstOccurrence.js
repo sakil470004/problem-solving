2af2e289aee60ab0ba82b4b181d67b1eacd5d733
Mynul Islam <mynul.sakil@gmail.com>
2024-07-08 21:01:23 +0600

    add findIndexOfTheFirstOccurrence

---
+++ LeetCode/findIndexOfTheFirstOccurrence.js
@@ -0,0 +1,6 @@
+var strStr = function(haystack, needle) {
+    const index = haystack.indexOf(needle);
+    return index;
+};
+const haystack = "leetcode", needle = "leeto";
+console.log(strStr(haystack, needle)); 
