cd1637d1c747917058cc1423b1f214197d8be944
Mynul Islam <mynul.sakil@gmail.com>
2024-07-13 17:01:57 +0600

    instructor method

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/14.arraysquire.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/14.arraysquire.js
@@ -9,13 +9,52 @@ const arraySquire = (arr) => {
       isNegative = true;
     }
     squires.push(arr[i] * arr[i]);
-
   }
-    if (!isNegative) {
-        return squires;
-    }
-    return squires.sort((a, b) => a - b);
+  if (!isNegative) {
+    return squires;
+  }
+  return squires.sort((a, b) => a - b);
 };
+// pointer method
+const arraySquire2 = (arr) => {
+  const resultArray = new Array(arr.length).fill(0);
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const leftSquire = arr[left] * arr[left];
+    const rightSquire = arr[right] * arr[right];
+    if (leftSquire > rightSquire) {
+      resultArray[right - left] = leftSquire;
+      left++;
+    } else {
+      resultArray[right - left] = rightSquire;
+      right--;
+    }
+  }
+  return resultArray;
+};
+// with instructor
+const arraySquire3 = (arr) => {
+  const newArray = new Array(arr.length).fill(0);
+  let leftPointer = 0;
+  let rightPointer = arr.length - 1;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const leftSquire = Math.pow(arr[leftPointer], 2);
+    const rightSquire = Math.pow(arr[rightPointer], 2);
+    if (leftSquire > rightSquire) {
+      newArray[i] = leftSquire;
+      leftPointer++;
+    } else {
+      newArray[i] = rightSquire;
+      rightPointer--;
+    }
+  }
+  return newArray;
+};
+const result = arraySquire([-4, -1, 0, 3, 10]); //time complexity: O(nlogn)//space complexity: O(n)
+const result2 = arraySquire2([-4, -1, 0, 3, 10]); //time complexity: O(n)//space complexity: O(n)
+const result3 = arraySquire3([-4, -1, 0, 3, 10]); //time complexity: O(n)//space complexity: O(n)
 
-const result = arraySquire([-4, -1, 0, 3, 10]);
 console.log(result);
+console.log(result2);
+console.log(result3);
