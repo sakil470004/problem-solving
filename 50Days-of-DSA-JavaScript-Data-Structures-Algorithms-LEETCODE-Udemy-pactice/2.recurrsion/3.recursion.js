fcb5bb9a7361303e19e31200b4aa3d76a0fadd29
Mynul Islam <mynul.sakil@gmail.com>
2024-07-15 23:56:43 +0600

    recursion

---
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/2.recurrsion/3.recursion.js
@@ -0,0 +1,11 @@
+// solve 5,4,3,2,1,1,2,3,4,5
+const recursiveFunction = (n) => {
+  if (n === 0) {
+    return;
+  }
+  console.log(n);
+  recursiveFunction(n - 1);
+  console.log(n);
+};
+const result = recursiveFunction(5);
+console.log(result);
