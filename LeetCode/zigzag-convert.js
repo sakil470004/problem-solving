6b4d9975abf13b8d411a6a332c099ca1f5304d13
Mynul Islam <mynul.sakil@gmail.com>
2024-07-17 14:34:56 +0600

    zigzag-convert

---
+++ LeetCode/zigzag-convert.js
@@ -0,0 +1,26 @@
+var convert = function (s, numRows) {
+  const len = s.length;
+  if (numRows === 1 || numRows >= len) return s;
+  let rowTrackerArray = Array(s).fill(-1);
+  let rowTracker = 0;
+  let reverse = false;
+  const resultArray=Array(numRows).fill('');
+  for (let i = 0; i < len; i++) {
+    rowTrackerArray[i] = rowTracker;
+    resultArray[rowTracker] += s[i];
+    if (rowTracker === 0) {
+      reverse = false;
+    } else if (rowTracker === numRows - 1) {
+      reverse = true;
+    }
+    if (reverse) {
+      rowTracker--;
+    } else {
+      rowTracker++;
+    }
+  }
+return resultArray.join('');
+};
+
+const result = convert("PAYPALISHIRING", 3);
+console.log(result); // PAHNAPLSIIGYIR
