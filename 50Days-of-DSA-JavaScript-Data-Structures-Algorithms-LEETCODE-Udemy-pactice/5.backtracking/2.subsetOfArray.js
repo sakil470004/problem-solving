beabab777aae7770424fdf298c97ea52c3ee9c4d
Mynul Islam <mynul.sakil@gmail.com>
2024-07-24 17:04:56 +0600

    added question

---
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/5.backtracking/2.subsetOfArray.js
@@ -0,0 +1,3 @@
+// ? Given an integer array of unique elements, return all possible subsets (the power set). The solution set must not contain duplicate subsets. return the solution in any order.
+
+// !test case=> [1,7] . powerset=[[],[1],[7],[1,7]]=>(2^n)
