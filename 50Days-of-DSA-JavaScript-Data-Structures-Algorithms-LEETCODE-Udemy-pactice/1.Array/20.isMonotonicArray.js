4bb4334bd70a13c3d1f74ac97e67cf18e425cfd5
Mynul Islam <mynul.sakil@gmail.com>
2024-07-26 19:30:14 +0600

    added question

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/1.Array/20.isMonotonicArray.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/1.Array/20.isMonotonicArray.js
@@ -1,10 +1,16 @@
+//* https://leetcode.com/problems/monotonic-array/
+// write down the question
+// isMonotonicArray
+// An array is monotonic if it is either monotone increasing or monotone decreasing.
+// An array A is monotone increasing if for all i <= j, A[i] <= A[j]. An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+// Return true if and only if the given array A is monotonic.
 const isMonoTonicArray = (arr) => {
   if (arr.length < 3) {
     return true;
   }
   let isIncreasing = false;
   let isDecreasing = false;
-  for (let i = 0; i < arr.length - 2; i++) {
+  for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i] > arr[i + 1]) {
       isDecreasing = true;
     } else if (arr[i] < arr[i + 1]) {
@@ -46,7 +52,7 @@ const isMonoTonicArray2 = (arr) => {
   return true;
 };
 
-const result = isMonoTonicArray([4, 4, 3, 2, 1]); //time complexity: O(n)//space complexity: O(1)
+const result = isMonoTonicArray([1, 3, 2]); //time complexity: O(n)//space complexity: O(1)
 console.log(result);
 const result2 = isMonoTonicArray2([3, 3, 3, 3, 3]); //time complexity: O(n)//space complexity: O(1)
 console.log(result2);
