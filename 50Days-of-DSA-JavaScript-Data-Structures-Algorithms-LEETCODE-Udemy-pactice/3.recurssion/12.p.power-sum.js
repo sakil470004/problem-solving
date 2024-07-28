0f9c45325de1a208797b1a7e8c92fe97eaeea8ce
Mynul Islam <mynul.sakil@gmail.com>
2024-07-28 15:05:57 +0600

    add my work

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/3.recurssion/12.p.power-sum.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/3.recurssion/12.p.power-sum.js
@@ -1,17 +1,18 @@
 // ? Let's define a peculiar type of array in which each element is either an integer or another peculiar array. Assume that a peculiar array is never empty. Write a function that will take a peculiar array as it's input and find the sum of it's elements. If an array is an element in the peculiar array you have to convert it to it's equivalent value so that you can sum it with the other elements. Equivalent value of an array is sum of it's elements raised to the number which represents how far nested it is. for e.g [2,3,[4,1,2]] = 2+3(4+1+2)^2 ,,, another [1,2,[7,[3,4],2]]=1+2+(7+(3+4)^3+2)^2,,,,
-function powerSum(array, power = 1) {
+
+const powerSum = (array, power = 1) => {
   let sum = 0;
-  //  ? loop the array. Which also have start and end condition. for recursion
+  //? loop the array . which is also have start and end condition for recursion
   array.forEach((item) => {
+    //* if it array recursive to the save function and make loop again else add to sum
     if (Array.isArray(item)) {
       sum += powerSum(item, power + 1);
     } else {
       sum += item;
     }
   });
-
-  return Math.pow(sum, power);
-}
+  return Math.pow(sum,power);
+};
 const compoundArray = [1, 2, 3, [4, 2], [[2]]];
 // ! calcluation look like [1+2+3+(4+2)^2+((2)^2)^3]
 const result = powerSum(compoundArray);
