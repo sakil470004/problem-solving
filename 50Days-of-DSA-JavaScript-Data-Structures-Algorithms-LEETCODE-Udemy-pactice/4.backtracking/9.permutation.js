02cc2f0b33286f60b2209d9a9b6fc54b0e41510e
Mynul Islam <mynul.sakil@gmail.com>
2024-07-29 21:54:14 +0600

    revise solved problem

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/4.backtracking/9.permutation.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/4.backtracking/9.permutation.js
@@ -1,5 +1,6 @@
 // ? given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 //! if array is given to us is => [1,2,3] the possible ans is [1,2,3] & [1,3,2] & [2,1,3] & [2,3,1] & [3,1,2] & [3,2,1]
+//* write a function that takes an array of distinct integers and returns all the possible permutations of the array.
 // for 3 number 3! for 4 number 4!;
 //? clarifying question.
 // ? What if nums is empty ?=> should I return an empty array
