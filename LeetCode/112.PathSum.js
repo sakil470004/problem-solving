79f1671a7f829988c66fe98852b6d168cf8fe948
Mynul Islam <mynul.sakil@gmail.com>
2024-08-13 01:21:51 +0600

    add pathsum

---
+++ LeetCode/112.PathSum.js
@@ -0,0 +1,13 @@
+// 112. Path Sum
+// Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+// A leaf is a node with no children.
+
+var hasPathSum = function (root, targetSum) {
+  if (!root) return false;
+  if (!root.left && !root.right && targetSum - root.val === 0) return true;
+  return (
+    hasPathSum(root.left, targetSum - root.val) ||
+    hasPathSum(root.right, targetSum - root.val)
+  );
+};
