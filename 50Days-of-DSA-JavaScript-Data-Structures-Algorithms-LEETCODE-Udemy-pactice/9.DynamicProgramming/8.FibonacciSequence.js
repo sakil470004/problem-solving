f47a4a1ca7370aaeb04987c0badb4de0198b6768
Mynul Islam <mynul.sakil@gmail.com>
2024-08-13 23:50:56 +0600

    add fibonacci sequence

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/9.DynamicProgramming/8.FibonacciSequence.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/9.DynamicProgramming/8.FibonacciSequence.js
@@ -8,7 +8,8 @@
 // 3. Bottom-Up Approach// Tabulation
 // 4. Bottom-Up Approach with Space Optimization
 
-var fib = function (n) {
+// memoization
+var fib3 = function (n) {
   let memo = {
     0: 0,
     1: 1,
@@ -35,6 +36,41 @@ var fib2 = function (n) {
 
   return fib2(n - 1) + fib2(n - 2);
 };
+// tabulation
+var fib = function (n) {
+  let dp = new Array(n + 1).fill(0);
+  dp[0] = 0;
+  if (n > 0) {
+    dp[1] = 1;
+  }
+  let count = 1;
+  while (count < n) {
+    count++;
+    dp[count] = dp[count - 1] + dp[count - 2];
+  }
+  return dp[n];
+};
+// space optimization tabulation
+var fib4 = function (n) {
+  if (n === 0 || n === 1) {
+    return n;
+  }
 
+  let prev = 0;
+
+  let curr = 1;
+
+  let count = 1;
+  while (count < n) {
+    count++;
+    let next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+  return curr;
+};
+
+console.log(fib4(41));
 console.log(fib(41));
+console.log(fib3(41));
 console.log(fib2(41));
