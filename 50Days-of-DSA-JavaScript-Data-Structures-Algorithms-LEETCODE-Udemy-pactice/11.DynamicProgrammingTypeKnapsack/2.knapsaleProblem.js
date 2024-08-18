fbe737d6d3562970b9b2b3148709af777d6dc105
Mynul Islam <mynul.sakil@gmail.com>
2024-08-19 02:17:06 +0600

    adding kanpsack problem question

---
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/11.DynamicProgrammingTypeKnapsack/2.knapsaleProblem.js
@@ -0,0 +1,4 @@
+// Question
+// 0 1 kanpsack problem
+// You are provided n items and each item has a weight and a value. You are also provided a knapsack that can hold a maximum weight of W. Your task is to select the items that you can put in the knapsack such that the total value of the items is maximized and the total weight is less than or equal to W. Items  are unique and indivisible, i.e., you can either take the whole item or leave it. You can not take a fraction of an item.
+//get leetcode question: https://leetcode.com/problems/maximum-value-of-an-item-in-a-knapsack-problem/
