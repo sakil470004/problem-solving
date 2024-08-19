28a0a8e49642342543f0069e8d4ef4568c2db00d
Mynul Islam <mynul.sakil@gmail.com>
2024-08-20 01:11:14 +0600

    adding summeryRanges

---
+++ LeetCode/228.SummaryRanges.js
@@ -0,0 +1,62 @@
+// 228. Summary Ranges
+// Easy
+// Topics
+// Companies
+// You are given a sorted unique integer array nums.
+
+// A range [a,b] is the set of all integers from a to b (inclusive).
+
+// Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+// Each range [a,b] in the list should be output as:
+
+// "a->b" if a != b
+// "a" if a == b
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,2,4,5,7]
+// Output: ["0->2","4->5","7"]
+// Explanation: The ranges are:
+// [0,2] --> "0->2"
+// [4,5] --> "4->5"
+// [7,7] --> "7"
+// Example 2:
+
+// Input: nums = [0,2,3,4,6,8,9]
+// Output: ["0","2->4","6","8->9"]
+// Explanation: The ranges are:
+// [0,0] --> "0"
+// [2,4] --> "2->4"
+// [6,6] --> "6"
+// [8,9] --> "8->9"
+ 
+
+var summaryRanges = function(nums) {
+    let result = [];
+    let i = 0;
+
+    while (i < nums.length) {
+        let start = i;
+        
+        // Iterate until the end of the current range
+        while (i + 1 < nums.length && nums[i + 1] === nums[i] + 1) {
+            i++;
+        }
+        
+        // If start and end are the same, it means single number
+        if (start === i) {
+            result.push(`${nums[start]}`);
+        } else {
+            // Else it's a range
+            result.push(`${nums[start]}->${nums[i]}`);
+        }
+        
+        // Move to the next number
+        i++;
+    }
+
+    return result;
+};
+console.log(summaryRanges([0,1,2,4,5,7]));//["0->2","4->5","7"]
