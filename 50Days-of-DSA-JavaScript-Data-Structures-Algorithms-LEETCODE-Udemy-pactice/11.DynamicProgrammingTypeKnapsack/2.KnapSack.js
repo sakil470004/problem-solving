dd73b10038db461c499f4bf996cb994d628f34e4
Mynul Islam <mynul.sakil@gmail.com>
2024-08-28 05:25:26 +0600

    adding knapSack

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/11.DynamicProgrammingTypeKnapsack/2.KnapSack.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/11.DynamicProgrammingTypeKnapsack/2.KnapSack.js
@@ -52,3 +52,20 @@ function KnapSack2(W, wt, val, n) {
   }
   return helper(0, W);
 }
+
+function knapsack(W,wt,val,n){
+  // n+1 rows and w+1 columns//fill with 0
+  const dp = Array.from({ length: n+1 }, () => Array(W + 1).fill(0));
+  for(let i=1;i<=n;i++){
+    for(let j=1;j<=W;j++){
+     const exclude = dp[i-1][j];
+      let include = 0;
+      // if weight of current item is less than or equal to current weight// wt and val array is 0 based index
+      if(wt[i-1]<=j){
+        include = val[i-1]+dp[i-1][j-wt[i-1]];
+      }
+      dp[i][j] = Math.max(include,exclude);
+    }
+  }
+  return dp[n][W];
+}
