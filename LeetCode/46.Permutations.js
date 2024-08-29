8f53046a6dd60b3abdef75b1bde56c8aef4fe3f4
Mynul Islam <mynul.sakil@gmail.com>
2024-08-29 20:06:40 +0600

    finish permute

---
+++ LeetCode/46.Permutations.js
@@ -0,0 +1,37 @@
+// 46. Permutations
+// Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+// Example 2:
+
+// Input: nums = [0,1]
+// Output: [[0,1],[1,0]]
+// Example 3:
+
+// Input: nums = [1]
+// Output: [[1]]
+function permute(nums) {
+    const result = [];
+
+    function backtrack(start) {
+        if (start === nums.length) {
+            result.push([...nums]);
+            return;
+        }
+
+        for (let i = start; i < nums.length; i++) {
+            [nums[start], nums[i]] = [nums[i], nums[start]]; // swap
+            backtrack(start + 1);
+            [nums[start], nums[i]] = [nums[i], nums[start]]; // backtrack
+        }
+    }
+
+    backtrack(0);
+    return result;
+}
+
+
+console.log(permute([1, 2, 3])); // [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
