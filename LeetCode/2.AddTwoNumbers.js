e3c40e46e79756a124cb3669bc0abe33a315a635
Mynul Islam <mynul.sakil@gmail.com>
2024-08-31 02:30:50 +0600

    update youtube video code

--- LeetCode/2.AddTwoNumbers.js
+++ LeetCode/2.AddTwoNumbers.js
@@ -16,6 +16,47 @@
 // Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 // Output: [8,9,9,9,0,0,0,1]
 
+
+
+
+
+// youtube video
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function addTwoNumbers(l1, l2) {
+    var dummy = new ListNode(0)
+    var current=dummy
+    var carry=0;
+ 
+    while(l1!=null || l2!=null || carry>0){
+     var val1=l1 ? l1.val : 0;
+     var val2=l2 ? l2.val : 0;
+     
+     var sum =val1+val2+carry
+     carry=Math.floor(sum/10);// floor(14/10) =1
+     sum%=10// 14%10=4
+ 
+     current.next=new ListNode(sum);
+     current =current.next
+    
+    if(l1) l1=l1.next
+    if(l2) l2= l2.next
+    }
+ 
+ return dummy.next
+ }
+ //finished youtube video code
+
 function ListNode(val, next = null) {
     this.val = val;
     this.next = next;
