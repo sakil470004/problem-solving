d4bf3aefcbb608fb024c1d70a64cdb760c848ecc
Mynul Islam <mynul.sakil@gmail.com>
2024-10-01 03:29:51 +0600

    adding combination sum 2

---
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/6.backtracking/109.combinations2.js
@@ -0,0 +1,47 @@
+// Coding Exercise: Combinations Sum 2
+// Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+// Each number in candidates may only be used once in the combination.
+
+// Note: The solution set must not contain duplicate combinations.
+
+// Example :
+
+// Input: candidates = [3,5,2,1,3], target = 7
+// Output: 
+// [
+// [1,3,3],
+// [5,2]
+// ]
+
+var combinationSum2 = function (candidates, target) {
+    let results = [];
+    const len = candidates.length;
+    candidates.sort((a, b) => a - b);
+    const helper = (index, currentSum, subset) => {
+        // base condition
+        // if currentSum
+        if (currentSum === target) {
+            results.push([...subset]);
+            return;
+        }
+        if (currentSum > target || index >= len - 1) {
+            // if current is bigger there is no need to go in this branch
+            return;
+        }
+
+        let hash = {};
+        for (let j = index; j < len; j++) {
+            // current number again
+            if (hash[candidates[j]]) continue;
+            hash[candidates[j]] = true
+            subset.push(candidates[j]);
+            helper(j + 1, currentSum + candidates[j], subset)
+            // include new number
+            subset.pop();
+        }
+
+    }
+    helper(0, 0, []);
+    return results;
+}
