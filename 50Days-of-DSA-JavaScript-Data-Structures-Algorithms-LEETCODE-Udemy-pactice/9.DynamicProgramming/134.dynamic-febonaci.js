1a09325f7bd1510b1d7fa63a19f445d90bc1bad0
Mynul Islam <mynul.sakil@gmail.com>
2024-10-10 04:10:16 +0600

    adding dynamic febonaci

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/9.DynamicProgramming/134.dynamic-febonaci.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/9.DynamicProgramming/134.dynamic-febonaci.js
@@ -45,5 +45,16 @@ var fib2 = function (n) {
 
 };
 
+// space optimized tabulation
+var fib3 = function (n) {
+    if (n <= 1) return n;
+    let a = 0, b = 1, c;
+    for (let i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
 
-console.log(fib2(41)); //0
+console.log(fib3(41)); //0
