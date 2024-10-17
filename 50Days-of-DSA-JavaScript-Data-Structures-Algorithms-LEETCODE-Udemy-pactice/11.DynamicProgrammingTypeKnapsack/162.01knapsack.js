4285726b90db70eb03dad93876a6bb6b50321cf3
Mynul Islam <mynul.sakil@gmail.com>
2024-10-17 11:07:29 -0930

    adding comment

---
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/11.DynamicProgrammingTypeKnapsack/162.01knapsack.js
@@ -0,0 +1,12 @@
+// Coding Exercise: 01 Knapsack
+// You are provided with a set of N items, each with a specified weight and value. Your objective is to pack these items into a backpack with a weight limit of W, maximizing the total value of items in the backpack. Specifically, you have two arrays: val[0..N-1], representing the values of the items, and wt[0..N-1], indicating their weights. Additionally, you have a weight limit W for the backpack. The challenge is to determine the most valuable combination of items where the total weight does not exceed W. Note that each item is unique and indivisible, meaning it must be either taken as a whole or left entirely.
+
+// Input:
+// N = 3
+// W = 8
+// values[] = [2,3,9]
+// weight[] = [8,2,5]
+// Output: 12
+// Explanation: Choose the last 2 items that weighs 2 and 5 units respectively and hold values 3 and 9 that add up to 12. 
+
+
