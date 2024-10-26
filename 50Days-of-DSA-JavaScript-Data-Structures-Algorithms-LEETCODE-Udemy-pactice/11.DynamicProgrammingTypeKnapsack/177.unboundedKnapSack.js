50290123a4d31d577fb405487ee97e645a3771f6
Mynul Islam <mynul.sakil@gmail.com>
2024-10-26 09:46:52 +0600

    update 177 unbonded

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/11.DynamicProgrammingTypeKnapsack/177.unboundedKnapSack.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/11.DynamicProgrammingTypeKnapsack/177.unboundedKnapSack.js
@@ -18,7 +18,7 @@
 // So, Total value = 2 + 2 + 2 = 6. and the total weight = 1 + 1 + 1  = 3 
 // which is <= 3.
 // recursive solution
-function knapSack(N, W, val, wt) {
+function knapSack1(N, W, val, wt) {
     // Base Case: If the knapsack weight limit is 0 or no items are left
     if (W === 0 || N === 0) {
         return 0;
@@ -48,5 +48,24 @@ function knapSack(N, W, val, wt) {
     // Calling the recursive function
     return knapSackRecursive(W, val, wt, N);
 }
+// tabulation
+function knapSack(N, W, val, wt) {
+    // dp array N+1 rows W+1 columns//fill dp with 0
+    const dp = Array.from({ length: N + 1 }, () => Array(W + 1).fill(0));
 
+    for (let i = 1; i <= N; i++) {
+        for (let j = 1; j <= W; j++) {
+            const exclude = dp[i - 1][j];
+            let include = 0;
+            // if weight of current item is less than or equal to current weight// wt and val array is 0 based index
+            if (wt[i - 1] <= j) {
+                include = val[i - 1] + dp[i][j - wt[i - 1]];
+            }
+            dp[i][j] = Math.max(include, exclude);
+        }
+    }
+    return dp[N][W];
+}
+
+console.log(knapSack1(2, 3, [4, 2], [3, 1])); // 3
 console.log(knapSack(2, 3, [4, 2], [3, 1])); // 3
