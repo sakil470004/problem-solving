0d22ad5726961e477884ad7da5480522dbe60900
Mynul Islam <mynul.sakil@gmail.com>
2024-11-09 00:56:28 +0600

    finished memo

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/13.dynamic-programming/197.edit-distanse.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/13.dynamic-programming/197.edit-distanse.js
@@ -18,6 +18,7 @@
 // dodse -> dose (remove 'd')
 // dose -> dos (remove 'e')
 
+//recursive solution
 var minDistance = function (word1, word2) {
     //Write Code here 
     let n = word1.length;
@@ -47,3 +48,42 @@ var minDistance = function (word1, word2) {
     return number_of_operations(0, 0);
 };
 
+//memoization
+var minDistance = function (word1, word2) {
+    //Write Code here 
+    let n = word1.length;
+    let m = word2.length;
+    let dp = Array.from({ length: n }, () => Array(m).fill(-1));
+    var number_of_operations = (index1, index2) => {
+        //   base case 
+        if (index1 > n - 1 && index2 > m - 1) {
+            return 0;
+        }
+        if (index1 > n - 1) {
+            return m - index2
+        }
+        if (index2 > m - 1) {
+            return n - index1;
+        }
+        // recursive case
+        if (dp[index1][index2] !== -1) {
+            return dp[index1][index2];
+        }
+        // ?equal
+        if (word1[index1] === word2[index2]) {
+            dp[index1][index2] = number_of_operations(index1 + 1, index2 + 1);
+        } else {
+            //not equal 
+            var insert = 1 + number_of_operations(index1, index2 + 1)
+            var deleteOp = 1 + number_of_operations(index1 + 1, index2)
+            var replace = 1 + number_of_operations(index1 + 1, index2 + 1)
+        }
+        dp[index1][index2] = Math.min(insert, deleteOp, replace)
+        return dp[index1][index2];
+    }
+    return number_of_operations(0, 0);
+};
+//tabulation
+var minDistance = function (word1, word2) {
+    
+}
