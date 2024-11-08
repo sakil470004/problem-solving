8db2ff0b1bd9643d40c34fc8a7f136f764153f61
Mynul Islam <mynul.sakil@gmail.com>
2024-11-09 01:11:56 +0600

    adding tabulation

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/13.dynamic-programming/197.edit-distanse.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/13.dynamic-programming/197.edit-distanse.js
@@ -85,5 +85,53 @@ var minDistance = function (word1, word2) {
 };
 //tabulation
 var minDistance = function (word1, word2) {
-    
+    let n = word1.length;
+    let m = word2.length;
+    let dp = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
+    // init with col and row values
+    for (let i = 0; i <= n; i++) {
+        dp[i][0] = i;
+    }
+    for (let j = 0; j <= m; j++) {
+        dp[0][j] = j;
+    }
+    for(let i =1 ;i<=n;i++){
+        for(let j=1;j<=m;j++){
+            if(word1[i-1]===word2[j-1]){
+                dp[i][j]=dp[i-1][j-1];
+            }else{
+                let insert = 1+dp[i][j-1];
+                let deleteOp = 1+dp[i-1][j];
+                let replace = 1+dp[i-1][j-1];
+                dp[i][j]=Math.min(insert,deleteOp,replace);
+            }
+        }
+    }
+    return dp[n][m];
+}
+// space optimized tabulation
+var minDistance = function (word1, word2) {
+    let n = word1.length;
+    let m = word2.length;
+    let dp = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
+    // init with col and row values
+    for (let i = 0; i <= n; i++) {
+        dp[i][0] = i;
+    }
+    for (let j = 0; j <= m; j++) {
+        dp[0][j] = j;
+    }
+    for(let i =1 ;i<=n;i++){
+        for(let j=1;j<=m;j++){
+            if(word1[i-1]===word2[j-1]){
+                dp[i][j]=dp[i-1][j-1];
+            }else{
+                let insert = 1+dp[i][j-1];
+                let deleteOp = 1+dp[i-1][j];
+                let replace = 1+dp[i-1][j-1];
+                dp[i][j]=Math.min(insert,deleteOp,replace);
+            }
+        }
+    }
+    return dp[n][m];
 }
