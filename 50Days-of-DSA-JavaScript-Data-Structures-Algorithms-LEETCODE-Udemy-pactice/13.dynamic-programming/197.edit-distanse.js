d2c8f756a197b98b9e7e824733e1dc697d68a66d
Mynul Islam <mynul.sakil@gmail.com>
2024-11-08 23:33:43 +0600

    edit dis

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/13.dynamic-programming/197.edit-distanse.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/13.dynamic-programming/197.edit-distanse.js
@@ -20,11 +20,30 @@
 
 var minDistance = function (word1, word2) {
     //Write Code here 
-    if (!word1 || !word1) {
-        return Math.max(word1.length, word2.length);    
+    let n = word1.length;
+    let m = word2.length;
+    var number_of_operations = (index1, index2) => {
+        //   base case 
+        if (index1 > n - 1 && index2 > m - 1) {
+            return 0;
+        }
+        if (index1 > n - 1) {
+            return m - index2
+        }
+        if (index2 > m - 1) {
+            return n - index1;
+        }
+        // recursive case
+        // ?equal
+        if (word1[index1] === word2[index2]) {
+            return number_of_operations(index1 + 1, index2 + 1);
+        }
+        //not equal 
+        var insert = 1 + number_of_operations(index1, index2 + 1)
+        var deleteOp = 1 + number_of_operations(index1 + 1, index2)
+        var replace = 1 + number_of_operations(index1 + 1, index2 + 1)
+        return Math.min(insert, deleteOp, replace)
     }
-    if (word1 === word2) {
-        return 0;
-    }
-    
+    return number_of_operations(0, 0);
 };
+
