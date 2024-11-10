c3453814dc16efb5273114a816f72b45ccd44da2
Mynul Islam <mynul.sakil@gmail.com>
2024-11-10 18:33:06 +0600

    adding all python code

---
+++ Code_with_harry/11.loop.py
@@ -0,0 +1,4 @@
+sakil ='''I am now using python for 2 years'''
+apple="apple is a important fruit"
+for char in sakil:
+    print(char)
---
+++ Code_with_harry/12.sring_slice.py
@@ -0,0 +1,4 @@
+name='Mynul Islam'
+print(name[0:5])
+print(name)
+print(len(name))
