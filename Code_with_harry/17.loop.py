1ec4ffacbf949f10effffe38c675388d7c0523e4
Mynul Islam <mynul.sakil@gmail.com>
2024-11-12 23:43:32 +0600

    range

--- Code_with_harry/17.loop.py
+++ Code_with_harry/17.loop.py
@@ -4,3 +4,12 @@ for color in colors:
     print(color)
     for char in color:
         print(char)
+
+for k in range(11):
+    print(k+1)
+
+for k in range(2,11):
+    print(k)
+
+for k in range(2,11,2):
+    print(k)
