afdcb52e364705548f613e28f034ca8389f38e70
Mynul Islam <mynul.sakil@gmail.com>
2024-11-13 00:03:06 +0600

    adding do while loop

---
+++ Code_with_harry/18.while.py
@@ -0,0 +1,11 @@
+i=0
+while(i<3):
+    print("Hello World",i)
+    i = i+1
+
+i=int(input("Enter a number: "))
+while(i<10):
+    i=int(input("Enter a number: "))
+    print(i)
+else:
+    print("You have entered a number greater than 10")
