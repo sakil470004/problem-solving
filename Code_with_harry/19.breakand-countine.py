d3a79d1fb3f16f9a21cab75abac5e4a014b0d5c3
Mynul Islam <mynul.sakil@gmail.com>
2024-11-13 19:39:36 +0600

    break and countinue

---
+++ Code_with_harry/19.breakand-countine.py
@@ -0,0 +1,9 @@
+for i in range(12):
+    if(i==5):
+        continue
+    if(i==11):
+        break
+    print("5x",i,"=",5*i)
+
+    # break out of loop
+    # continue to next iteration. or out of current iteration.
