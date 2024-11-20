2c0f45e344f462d728cb4b93aed687fe03f9ca63
Mynul Islam <mynul.sakil@gmail.com>
2024-11-20 20:07:29 +0600

    fixed using set

--- LeetCode/1657. Determine if Two Strings Are Close.js
+++ LeetCode/1657. Determine if Two Strings Are Close.js
@@ -34,23 +34,26 @@
 
 var closeStrings = function(word1, word2) {
     if (word1.length !== word2.length) return false;
-    let obj1 = {};
-    let obj2 = {};
-    for(let i=0;i<word1.length;i++){
-        obj1[word1[i]] = obj1[word1[i]] + 1 || 1;
-        obj2[word2[i]] = obj2[word2[i]] + 1 || 1;
-    }
-    let array1=[]
-    let array2=[]
-    for (const key in obj1) {
-        array1.push(obj1[key]);
-    }
-    array1.sort((a,b)=>a-b);
-    for (const key in obj2) {
-        array2.push(obj2[key]);
-    }
-    array2.sort((a,b)=>a-b);
     
+    let obj1 = {}, obj2 = {};
+    let set1 = new Set(word1);
+    let set2 = new Set(word2);
 
+    // Check if both strings have same characters
+    for(let char of set1) {
+        if(!set2.has(char)) return false;
+    }
+
+    // Count frequencies
+    for(let i = 0; i < word1.length; i++) {
+        obj1[word1[i]] = (obj1[word1[i]] || 0) + 1;
+        obj2[word2[i]] = (obj2[word2[i]] || 0) + 1;
+    }
+
+    // Compare frequency patterns
+    let freq1 = Object.values(obj1).sort((a,b) => a-b);
+    let freq2 = Object.values(obj2).sort((a,b) => a-b);
+    
+    return freq1.join('') === freq2.join('');
 };
-console.log(closeStrings("cabbba","caabbb"));//true
+console.log(closeStrings("a","b"));//true
