c9c5253fa3d864062524c371cd6199947aba249d
Mynul Islam <mynul.sakil@gmail.com>
2024-11-20 22:18:12 +0600

    fix bug for determine two string are close

--- LeetCode/1657. Determine if Two Strings Are Close.js
+++ LeetCode/1657. Determine if Two Strings Are Close.js
@@ -6,7 +6,7 @@
 // Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
 // For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
 // You can use the operations on either string as many times as necessary.
-
+ 
 // Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
 
  
