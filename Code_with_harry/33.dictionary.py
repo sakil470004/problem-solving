64ef55f4844d7eadc35a227dc2aaa6ab40dd4d6b
Mynul Islam <mynul.sakil@gmail.com>
2024-11-22 03:00:47 +0600

    adding python dictionary

---
+++ Code_with_harry/33.dictionary.py
@@ -0,0 +1,15 @@
+disc={
+    "name":"Mynul Islam",
+    "age":22,
+    "fav_movies":["coco","kimi no na wa"],
+}
+print(disc)
+print(disc["name"])
+print(disc["fav_movies"])
+disc["hobbies"]=["reading","coding"]
+print(disc)
+print(disc.get("name2"))# it will return None if the key is not present in the dictionary
+print(disc.get("name2","Not Found")) # it will return the value of the key if the key is present in the dictionary
+# print(disc["name2"]) # it will give error if the key is not present in the dictionary
+print(disc.keys())
+print(disc.values())
