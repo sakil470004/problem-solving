45b9f3abc8368961e703b5972fc98f6a1981e505
Mynul Islam <mynul.sakil@gmail.com>
2024-12-08 21:40:56 +0600

    recursive lis

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/14.dynamic-programming-lis/211.recursive-lts.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/14.dynamic-programming-lis/211.recursive-lts.js
@@ -14,25 +14,41 @@
 // therefore the length is 4.
 
 // mynul solution// is not working
-var lengthOfLIS = function(nums) {
+var lengthOfLIS = function (nums) {
     //Write code here 
-    let result=0;
-    let n=nums.length;
-    let helper=(nums,index,prev,total)=>{
-        if(index===n-1){
-            result=Math.max(result,total);
+    let result = 0;
+    let n = nums.length;
+    let helper = (nums, index, prev, total) => {
+        if (index === n - 1) {
+            result = Math.max(result, total);
             return;
         }
         // include
-        if(nums[index]>prev){
-            helper(nums,index+1,nums[index],total+1);
+        if (nums[index] > prev) {
+            helper(nums, index + 1, nums[index], total + 1);
         }
         // exclude
-        helper(nums,index+1,prev,total);
+        helper(nums, index + 1, prev, total);
     }
-    helper(nums,0,0,0);
+    helper(nums, 0, 0, 0);
     return result;
-     
+
 };
 
-console.log(lengthOfLIS([300,9,2,5,3,7,500,400])); //4
+var lengthOfLIS = function (nums) {
+    let n = nums.length;
+    let helper = (curr, prev) => {
+        if(curr >= n) return 0;
+        // recursive case
+        let exclude = helper(curr + 1, prev);
+
+        let include = 0;
+        if(prev===-1 ||nums[curr] > prev) {
+            include = 1 + helper(curr + 1, nums[curr]);
+        }
+        return Math.max(include, exclude);
+
+    }
+    return helper(0, -1);
+}
+console.log(lengthOfLIS([300, 9, 2, 5, 3, 7, 500, 400])); //4
