525afa344421af0c01bc03ae68c46296f3a2c180
Mynul Islam <mynul.sakil@gmail.com>
2024-12-22 23:01:11 +0600

    adding comments

--- LeetCode/714. Best Time to Buy and Sell Stock with Transaction Fee.js
+++ LeetCode/714. Best Time to Buy and Sell Stock with Transaction Fee.js
@@ -8,18 +8,18 @@
 
 // You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
 // The transaction fee is only charged once for each stock purchase and sale.
- 
 
 // Example 1:
 
 // Input: prices = [1,3,2,8,4,9], fee = 2
 // Output: 8
-// Explanation: The maximum profit can be achieved by:
+// Explanations: The maximum profit can be achieved by:
 // - Buying at prices[0] = 1
 // - Selling at prices[3] = 8
 // - Buying at prices[4] = 4
 // - Selling at prices[5] = 9
 // The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
+
 // Example 2:
 
 // Input: prices = [1,3,7,5,10,3], fee = 3
