70d99b8bdc79e1d18bcf4c9ffd533fac1f919356
Mynul Islam <mynul.sakil@gmail.com>
2025-01-03 00:33:41 +0600

    russian doll

---
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/14.dynamic-programming-lis/238.Russian Doll Envelopes.js
@@ -0,0 +1,19 @@
+// Udemy=>Jackson Kailath Teacher's course=> 50days leetcode=> module 14=> LTS pair chain Russian Doll Envelopes
+// You are given a 2D array of integers envelopes where envelopes[i] = [wi, hi] represents the width and the height of an envelope.
+
+// One envelope can fit into another if and only if both the width and height of one envelope are greater than the other envelope's width and height.
+
+// Return the maximum number of envelopes you can Russian doll (i.e., put one inside the other).
+
+// Note: You cannot rotate an envelope.
+
+// Example :
+
+// Input: envelopes = [[6,5],[7,5],[7,8],[3,4]]
+// Output: 3
+// Explanation: The maximum number of envelopes you can Russian doll is 3 ([3,4] => [6,5] => [7,8]).
+
+var maxEnvelopes = function(envelopes) {
+    //Write code here 
+    
+};
