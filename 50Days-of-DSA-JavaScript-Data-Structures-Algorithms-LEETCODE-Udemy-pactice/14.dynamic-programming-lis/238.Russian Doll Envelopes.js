d72c5e24e9d43f8ae8512e2840f665214259b07e
Mynul Islam <mynul.sakil@gmail.com>
2025-02-15 21:37:38 +0600

    added my approch russian doll

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/14.dynamic-programming-lis/238.Russian Doll Envelopes.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/14.dynamic-programming-lis/238.Russian Doll Envelopes.js
@@ -12,11 +12,30 @@
 // Input: envelopes = [[6,5],[7,5],[7,8],[3,4]]
 // Output: 3
 // Explanation: The maximum number of envelopes you can Russian doll is 3 ([3,4] => [6,5] => [7,8]).
-
+// mynul approach
 var maxEnvelopes = function(envelopes) {
     //Write code here 
+    envelopes.sort((a,b) => a[0] - b[0]);
+   
+    let helper =(index,total,lastElementsIndex) => {
+        // base case
+        if(index===envelopes.length){
+            return total;
+        }
+        // include the current envelope
+        let max=0;
+        if(lastElementsIndex===-1 || (envelopes[index][0]>envelopes[lastElementsIndex][0] && envelopes[index][1]>envelopes[lastElementsIndex][1])){
+          max= helper(index+1,total+1,index);
+        }
+        // exclude the current envelope
+
+        let max2= helper(index+1,total,lastElementsIndex);
+        return Math.max(max,max2);
+    }
+   return  helper(0,0,-1);
     
 };
+console.log(maxEnvelopes([[6,5],[7,5],[7,8],[3,4]]));//3
 
 
 
