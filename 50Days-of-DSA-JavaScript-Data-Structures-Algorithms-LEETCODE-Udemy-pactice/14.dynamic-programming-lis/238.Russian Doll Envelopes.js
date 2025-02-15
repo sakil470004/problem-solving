58bc6e8ed4d04021ca339b8fd7cd2970467bbfb9
Mynul Islam <mynul.sakil@gmail.com>
2025-02-15 21:56:00 +0600

    adding instructor approach

--- 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/14.dynamic-programming-lis/238.Russian Doll Envelopes.js
+++ 50Days-of-DSA-JavaScript-Data-Structures-Algorithms-LEETCODE-Udemy-pactice/14.dynamic-programming-lis/238.Russian Doll Envelopes.js
@@ -35,8 +35,63 @@ var maxEnvelopes = function(envelopes) {
    return  helper(0,0,-1);
     
 };
+// insturctor approach 1 LTS
+var maxEnvelopes = function(envelopes) {
+    // if width is same then sort by height in descending order
+    envelopes.sort((a,b) => a[0] === b[0]? b[1]-a[1]:a[0]-b[0]);
+    const n=envelopes.length;
+    const dp=new Array(n).fill(1);
+    let max=1;
+    for(let i=1;i<n;i++){
+        for(let j=0;j<i;j++){
+            if(envelopes[i][1]>envelopes[j][1 ] && dp[j]+1>dp[i]){
+                dp[i]=dp[j]+1;
+            }
+        }
+        if(dp[i]>max){
+
+            max=Math.max(max,dp[i]);
+        }
+    }
+    return max;
+}
 console.log(maxEnvelopes([[6,5],[7,5],[7,8],[3,4]]));//3
 
+// insturctor approach 2 BINARY SEARCH
+var maxEnvelopes = function(envelopes) {
+    envelopes.sort((a, b) => a[0] === b[0] ? b[1] - a[1] : a[0] - b[0]);
+ 
+    const n = envelopes.length;
+    const sub = [envelopes[0][1]];
+ 
+    function binarySearch(sub, num) {
+        let left = 0, right = sub.length;
+        while (left < right) {
+            let mid = Math.floor((left + right) / 2);
+            if (num > sub[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+ 
+    for (let i = 1; i < n; i++) {
+        const num = envelopes[i][1];
+        if (num > sub[sub.length - 1]) {
+            sub.push(num);
+        } else {
+            const x = binarySearch(sub, num);
+            sub[x] = num;
+        }
+    }
+    return sub.length;
+};
+ 
+// Example usage:
+// const envelopes = [[5,4],[6,4],[6,7],[2,3]];
+// console.log(maxEnvelopes(envelopes)); // Output: 3
 
 
 
