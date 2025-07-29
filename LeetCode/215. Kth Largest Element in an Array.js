02a674e8f8dae31d977f3dae9d7bc8dc9a640b6b
Mynul Islam <mynul.sakil@gmail.com>
2025-07-30 00:25:40 +0600

    update problem 215

--- LeetCode/215. Kth Largest Element in an Array.js
+++ LeetCode/215. Kth Largest Element in an Array.js
@@ -23,9 +23,29 @@
 
 // 1 <= k <= nums.length <= 105
 // -104 <= nums[i] <= 104
+// 1st solution using sorting
+// var findKthLargest = function(nums, k) {
+//     nums.sort((a,b)=>(b-a));
+//     const number=nums[k-1];
+//     return number;
+// };
 
-var findKthLargest = function(nums, k) {
-    nums.sort((a,b)=>(b-a));
-    const number=nums[k-1];
-    return number;
-};
+function findKthLargest(nums, k) {
+    const minHeap = [];
+
+    // Insert number into heap and keep heap size <= k
+    for (let num of nums) {
+        minHeap.push(num);
+        minHeap.sort((a, b) => a - b); // Maintain min-heap property
+
+        if (minHeap.length > k) {
+            minHeap.shift(); // Remove smallest
+        }
+    }
+
+    return minHeap[0]; // The kth largest element
+}
+
+
+console.log(findKthLargest([3,2,1,5,6,4], 2)); // Output: 5
+console.log(findKthLargest([3,2,3,1,2,4,5,5,6], 4)); // Output: 4
