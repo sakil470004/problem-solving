0489839e9380e1748eebd6c181cabbe8b1399204
Mynul Islam <mynul.sakil@gmail.com>
2025-08-27 18:05:54 +0600

    updated about today test prep

--- interview/27-08-25/test.js
+++ interview/27-08-25/test.js
@@ -1,2 +1,16 @@
 console.log(x); // undefined
 var x = 5;
+// 4. What is a closure?
+
+// A function that “remembers” variables from its outer scope even after the outer function has finished.
+
+function outer() {
+  let count = 0;
+  return function inner() {
+    count++;
+    return count;
+  };
+}
+const counter = outer();
+counter(); // 1
+counter(); // 2
