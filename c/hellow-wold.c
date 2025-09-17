a4b7b86ddfda2ccf6efbef4d17b59047214bd4d4
Mynul Islam <mynul.sakil@gmail.com>
2025-09-17 19:58:24 +0600

    update

--- c/hellow-wold.c
+++ c/hellow-wold.c
@@ -1,12 +1,12 @@
-#include <stdio.h>  // for printf and scanf
+#include <stdio.h> // for printf and scanf
 
-int main() {
-    char name[50];  // variable to store input (string)
+int main()
+{
+    char name[50]; // variable to store input (string)
 
-    printf("Enter your name: ");     // ask the user
-    scanf("%49s", name);             // read input (up to 49 characters, avoid overflow)
+    printf("Enter your name: "); // ask the user
+    scanf("%49s", name);         // read input (up to 49 characters, avoid overflow)
 
     printf("Hello, %s! Welcome to C programming.\n", name); // print with formatting
-
     return 0;
 }
