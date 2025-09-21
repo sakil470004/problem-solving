d34e42798b4b3b1d8e35d66894d34d2371bb5a28
Mynul Islam <mynul.sakil@gmail.com>
2025-09-21 08:19:08 +0600

    adding vaild quesiton

---
+++ interview/codding-pb/valid-panthatis.js
@@ -0,0 +1,19 @@
+// 🔹 Problem 3: Valid Parentheses
+
+// Q: Check if parentheses in a string are valid.
+
+function isValid(s) {
+  const stack = [];
+  const map = { ")": "(", "}": "{", "]": "[" };
+  
+  for (let char of s) {
+    if (char in map) {
+      if (stack.pop() !== map[char]) return false;
+    } else {
+      stack.push(char);
+    }
+  }
+  return stack.length === 0;
+}
+console.log(isValid("()[]{}")); // true
+console.log(isValid("(]"));     // false
