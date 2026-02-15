1d95cf2279fab32317bc9bcfa622762f9774e9e8
Mynul Islam <mynul.sakil@gmail.com>
2026-02-15 22:35:01 +0600

    added in

---
+++ interview/quesitons/16_02_26 _infinite rerender.js
@@ -0,0 +1,37 @@
+// 24. Debug Scenario: Why is this component re-rendering infinitely?
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, [data]);
+
+  const fetchData = () => {
+    setData([1, 2, 3]);
+  };
+
+  return <div>{data.length}</div>;
+}
+
+
+// Answer:
+// Answer:
+
+// useEffect depends on data
+
+// fetchData() updates data
+
+// Update triggers re-render
+
+// Re-render triggers useEffect again
+
+// Infinite loop.
+
+// Fix:
+
+useEffect(() => {
+  fetchData();
+}, []);
+
+
+// Or restructure logic.
