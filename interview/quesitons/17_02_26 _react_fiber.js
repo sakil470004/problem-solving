aeb2f488bc6d4058a8f50ce9e6cbcdf2a8a509fd
Mynul Islam <mynul.sakil@gmail.com>
2026-02-17 22:19:12 +0600

    updated

---
+++ interview/quesitons/17_02_26 _react_fiber.js
@@ -0,0 +1,27 @@
+// 25. Explain React Fiber in simple terms.
+
+
+
+// Answer:
+
+// React Fiber is React’s internal rendering engine.
+
+
+
+// It allows:
+
+// - Breaking rendering work into small units
+
+// - Pausing and resuming rendering
+
+// - Prioritizing updates
+
+
+
+// Why it matters:
+
+// - Smooth UI
+
+// - Concurrent rendering
+
+// - Better performance under heavy load
