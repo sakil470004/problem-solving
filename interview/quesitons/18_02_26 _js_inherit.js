0783285d82aae7fcb0b581b28b138d1cbc87e540
Mynul Islam <mynul.sakil@gmail.com>
2026-02-18 21:54:31 +0600

    modified and tested for me

--- interview/quesitons/18_02_26 _js_inherit.js
+++ interview/quesitons/18_02_26 _js_inherit.js
@@ -4,15 +4,29 @@
 // In JavaScript, objects inherit from other objects.
 
 const person = {
-  greet() {
-    console.log("Hello");
-  }
+    
+    greet() {
+        console.log("Hello");
+    },
+    greetWithName(name) {
+        console.log(`Hello ${name}`);
+    }
 };
 
 const user = Object.create(person);
 user.greet(); // Hello
+const user2 = Object.create(person);
+user2.greetWithName("Alice"); // Hello Alice
 
 
 // user inherits from person.
 
 // Everything in JS is based on prototype chains.
+
+// Why it matters:
+
+// - Code reuse
+
+// - Dynamic behavior
+
+// - Flexible object creation
