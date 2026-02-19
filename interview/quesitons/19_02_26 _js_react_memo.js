28e5ae66a00b2af6b40f1d0b0e044e4e67729cdf
Mynul Islam <mynul.sakil@gmail.com>
2026-02-19 21:16:10 +0600

    adding react momo questions

---
+++ interview/quesitons/19_02_26 _js_react_memo.js
@@ -0,0 +1,20 @@
+// 27. React Performance Question:
+
+// When would you use React.memo?
+
+// Answer:
+// Use it when:
+
+// Component renders frequently
+
+// Props rarely change
+
+// Rendering is expensive
+
+const Child = React.memo(({ value }) => {
+  console.log("Rendered");
+  return <div>{value}</div>;
+});
+
+
+// If value doesn’t change → no re-render.
