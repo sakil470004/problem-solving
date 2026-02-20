0173212b84bb13ecf98458bba1ad09f10b312e99
Mynul Islam <mynul.sakil@gmail.com>
2026-02-20 22:59:04 +0600

    update throttle js

---
+++ interview/quesitons/20_02_26 _throttle.js
@@ -0,0 +1,23 @@
+// 28. Coding Challenge – Implement Throttle
+
+// Answer:
+
+function throttle(fn, delay) {
+  let lastCall = 0;
+
+  return function (...args) {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      fn(...args);
+    }
+  };
+}
+
+const handleScroll = throttle(() => {
+  console.log("Scroll event handled");
+}, 1000);
+
+window.addEventListener("scroll", handleScroll);
+
+//  
