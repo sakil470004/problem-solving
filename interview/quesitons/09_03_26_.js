08f77822fef669ed39a008d604e4aaa377bc627f
Mynul Islam <mynul.sakil@gmail.com>
2026-03-09 23:19:29 +0600

    update

---
+++ interview/quesitons/09_03_26_.js
@@ -0,0 +1,24 @@
+46. What is the difference between call, apply, and bind?
+
+Answer:
+
+//! They control this.
+
+function greet() {
+  console.log(this.name);
+}
+
+const user = { name: "Mynul" };
+
+greet.call(user);   // Mynul
+greet.apply(user);  // Mynul
+const newFn = greet.bind(user);
+newFn();            // Mynul
+
+//? Difference:
+//? call → arguments separated
+//? apply → arguments as array
+//? bind → returns new function
+
+
+
