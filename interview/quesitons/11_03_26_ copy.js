85349dc30deb4b26113e2f7f86d62b9ea44627d5
Mynul Islam <mynul.sakil@gmail.com>
2026-03-12 23:00:20 +0600

    add time complexity

---
+++ interview/quesitons/11_03_26_ copy.js
@@ -0,0 +1,16 @@
+
+// 49. What is Time Complexity of common JS operations?
+
+
+
+//! | Operation     | Complexity |
+//? | ------------- | ---------- |
+//? | Array push    | O(1)       |
+//? | Array unshift | O(n)       |
+//? | Array find    | O(n)       |
+//? | Object access | O(1)       |
+//? | Sorting       | O(n log n) |
+
+//! Knowing this avoids bad design decisions.
+
+
