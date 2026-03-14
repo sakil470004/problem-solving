0b51f39f52738324c2d1a9c25eefdaad20279250
Mynul Islam <mynul.sakil@gmail.com>
2026-03-14 23:26:54 +0600

    updated

--- interview/quesitons/14_03_26_.js
+++ interview/quesitons/14_03_26_.js
@@ -2,8 +2,8 @@
 //! Q. What will be the output?
 //? console.log(typeof null);
 
-//? Answer:
-//? object
+//Answer:
+// object
 
 //? Explanation:
 //? This is a long-standing JavaScript bug from the early days of the language. null is a primitive value, but typeof null returns "object" due to legacy implementation.
