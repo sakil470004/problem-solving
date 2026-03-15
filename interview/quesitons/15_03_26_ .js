b69aa7d5a74a7c1ea13175a5993c8c5e625d5328
Mynul Islam <mynul.sakil@gmail.com>
2026-03-16 02:41:55 +0600

    updated

---
+++ interview/quesitons/15_03_26_ .js
@@ -0,0 +1,12 @@
+/*
+
+? Q. What will be the output?
+!=> console.log([] + []);
+
+Answer:
+""
+Explanation:
+? Both arrays convert to empty strings when coerced to primitives, so it becomes "" + "".
+
+
+*/
